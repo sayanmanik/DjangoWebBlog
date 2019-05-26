ac68a4bdf71fcba5f349dd6b8017c248f7d637e0
unknown <sayanmanik.sm@gmail.com>
2019-05-26 20:20:28 +0530

    Registration Feature Added

--- blog/models.py
+++ blog/models.py
@@ -11,5 +11,5 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete = models.CASCADE)
     
 
-def __str__(self):
-    return self.title
+#def __str__(self):
+#    return self.title
