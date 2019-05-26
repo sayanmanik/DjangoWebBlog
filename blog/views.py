97bcb9d892f0e08cdccde4309020d100d6cc157e
unknown <sayanmanik.sm@gmail.com>
2019-05-26 09:34:38 +0530

    Database Added added

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts = [
-
-    {
-        'author' : 'Sayan Manik',
-        'title' : 'Blog post 1',
-        'content' : 'First Post Content',
-        'date_posted' : 'May 25, 2019'
-    },
-
-    {
-        'author' : 'Corey MS',
-        'title' : 'Blog post 2',
-        'content' : 'Second Post Content',
-        'date_posted' : 'May 25, 2019'
-    },
-]
 
 def home(request):
     contexts = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blogs/home.html',contexts)
 
